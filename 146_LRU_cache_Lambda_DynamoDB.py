01b7d28bef4778f3c1ab2c122ff590fa9d55624e
Рамиль Хамидуллин <ramilhamidullin@Ramils-MacBook-Pro.local>
2025-03-31 16:39:46 -0700

    e

--- 146_LRU_cache_Lambda_DynamoDB.py
+++ 146_LRU_cache_Lambda_DynamoDB.py
@@ -1,2 +1,18 @@
+from collections import OrderedDict
+
 class LRUCache(object):
-    def __init__(self,capacity)
+    def __init__(self,capacity:int):
+        self.cache=OrderedDict()
+        self.capacity =capacity
+    
+    def get (self,key):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+    
+    def put(self,key, value):
+        if key in self.cache:
+            self.cache[key]=key
+            
+    
